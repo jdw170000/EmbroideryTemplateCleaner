0a3df31848be0570c3eecdc06081386fad293f1d
jdw170000 <jdw170000@utdallas.edu>
2025-08-05 17:50:34 -0500

    add version number to program title

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import tkinter as tk
 from gui import CleanerGUI
 from config import load_config, save_config
 
+VERSION = 'v1.1.2'
+
 def main():
     root = tk.Tk()
-    root.title("Embroidery Template Cleaner")
+    root.title(f"Embroidery Template Cleaner {VERSION}")
     config = load_config()
     app = CleanerGUI(master=root, config=config)
     app.pack(padx=10, pady=10)
